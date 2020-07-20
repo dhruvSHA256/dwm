1269d8ab1564ab693ee91373160814795d373a8e
Dhruv SHarma <dhruv.sah256@gmail.com>
2020-07-20 05:39:22 +0530

    changed keybindings

--- config.h
+++ config.h
@@ -8,15 +8,15 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int user_bh            = 30;        /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
 static const int statuspad	    = 15;
-static const char *fonts[]          = { "JetBrainsMono Nerd Font Mono:style=Regular:pixelsize=14:antialias=true:autohint=true","JoyPixels:pixelsize=18:style=Regular:antialias=true:autohint=true"};
+static const char *fonts[]          = { "JetBrainsMono Nerd Font Mono:style=Regular:pixelsize=12:antialias=true:autohint=true","JoyPixels:pixelsize=10:style=Regular:antialias=true:autohint=true"};
 static const char dmenufont[]       = { "JetBrainsMono Nerd Font:pixelsize=16:antialias=true:autohint=true" };
 static const unsigned int gappih    = 15;       /* horiz inner gap between windows */
 static const unsigned int gappiv    = 15;       /* vert inner gap between windows */
 static const unsigned int gappoh    = 15;       /* horiz outer gap between windows and screen edge */
 static const unsigned int gappov    = 15;       /* vert outer gap between windows and screen edge */
 static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
-static const int vertpad            = 0;       /* vertical padding of bar */
-static const int sidepad            = 0;       /* horizontal padding of bar */
+static const int vertpad            = 3;       /* vertical padding of bar */
+static const int sidepad            = 3;       /* horizontal padding of bar */
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -88,11 +88,11 @@ static const int resizehints = 1;    /* 1 means respect size hints in tiled resi
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
+	{ " (@) ",      spiral }, // fibonacci spiral
 	{ " [] ",      tile },    /* first entry is default */
 	{ " <> ",      NULL },    /* no layout function means floating behavior */
 	{ " [M] ",      monocle },
-	{ " [\] ",     dwindle }, // fibonacci dwindle
-	{ " (@) ",      spiral }, // fibonacci spiral
+	{ " [/\] ",     dwindle }, // fibonacci dwindle
 	{ "TTT",      bstack},
 	{"|M|",      centeredmaster}, 
 	{ "[D]",      deck},
@@ -134,15 +134,13 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_Return, zoom,           {0} },
 	{ Mod1Mask,                       XK_Tab,    view,           {0} },
 	{ MODKEY,                       XK_q,      killclient,     {0} },
-	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
-	{ MODKEY,                       XK_r,      setlayout,      {.v = &layouts[3]} },
-	{ MODKEY|ShiftMask,             XK_r,      setlayout,      {.v = &layouts[4]} },
+//	{ MODKEY|ShiftMask,                       XK_t,      setlayout,      {.v = &layouts[0]} },
+//	{ MODKEY|ShiftMask,                       XK_f,      setlayout,      {.v = &layouts[1]} },
+//	{ MODKEY|ShiftMask,                       XK_m,      setlayout,      {.v = &layouts[2]} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
 	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
-	{ MODKEY|ShiftMask,             XK_n,      togglealttag,   {0} },
+//	{ MODKEY|ShiftMask,             XK_n,      togglealttag,   {0} },
 
 	{ MODKEY,                       XK_Down,   moveresize,     {.v = "0x 10y 0w 0h" } },
 	{ MODKEY,                       XK_Up,     moveresize,     {.v = "0x -10y 0w 0h" } },
