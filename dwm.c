ba7e8cefa9a18a1a9e6703f9d46a2f4df9f8e0f2
DHruv <dhruv.sha256@gmail.com>
2020-08-29 19:38:56 +0530

    updated cycleview

--- dwm.c
+++ dwm.c
@@ -1849,7 +1849,11 @@ void incrivgaps(const Arg *arg) {
 
 void setlayout(const Arg *arg) {
   if (!arg || !arg->v || arg->v != selmon->lt[selmon->sellt])
-    selmon->sellt ^= 1;
+  { 
+    Layout *l;
+    for (l = (Layout *)layouts; l != selmon->lt[selmon->sellt]; l++);
+    selmon->lt[selmon->sellt] = l+1;
+  }
   if (arg && arg->v)
     selmon->lt[selmon->sellt] = (Layout *)arg->v;
   strncpy(selmon->ltsymbol, selmon->lt[selmon->sellt]->symbol,
@@ -2192,7 +2196,6 @@ void toggleview(const Arg *arg) {
 void cycleview(const Arg *arg) {
   unsigned int j,  occ = 0 , currtag = selmon->tagset[selmon->seltags];
   Client *c;
-  Arg argm;
 
   for (c = selmon->clients; c; c = c->next)
     occ |= c->tags == 255 ? 0 : c->tags;
@@ -2200,12 +2203,10 @@ void cycleview(const Arg *arg) {
   if (arg->i == 0) {
     for (j = currtag >> 1 ; j >= 0; j >>=1) {
       if (j <= 0) {
-        argm.ui = 1 << 8 ;
-        view(&argm);
+        view((&((Arg){.ui = (1 << 8)})));
         return;
       } else {
-        argm.ui = j;
-        view(&argm);
+        view((&((Arg){.ui = (j)})));
         return;
       }
     }
@@ -2214,12 +2215,10 @@ void cycleview(const Arg *arg) {
   if (arg->i == 1) {
     for (j = currtag << 1 ; j <= 1 << 9; j <<=1 ) {
       if (j > 1 << 8 ) {
-        argm.ui = 1 << 0;
-        view(&argm);
+        view((&((Arg){.ui = (1 << 0)})));
         return;
       } else {
-        argm.ui = j;
-        view(&argm);
+        view((&((Arg){.ui = (j)})));
         return;
       }
     }
@@ -2228,8 +2227,7 @@ void cycleview(const Arg *arg) {
   if (arg->i == 3) {
     for (j = currtag >> 1 ; j >0; j >>=1) {
       if (occ & j) {
-        argm.ui = j;
-        view(&argm);
+        view((&((Arg){.ui = (j)})));
         return;
       } else {
         continue;
@@ -2240,8 +2238,7 @@ void cycleview(const Arg *arg) {
   if (arg->i == 4) {
     for (j = currtag << 1 ; j <= 1 << 8; j <<=1 ) {
       if (occ & j ) {
-        argm.ui = j;
-        view(&argm);
+        view((&((Arg){.ui = (j)})));
         return;
       } else {
         continue;
